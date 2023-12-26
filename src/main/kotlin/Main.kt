149afdb80a2f6e1d70f4dd588c3e94f632479aee
sdram58 <sdram58@hotmail.com>
2023-12-26 19:18:19 +0100

    Second Commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,9 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -10,22 +12,47 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
+
+class AppState{
+    val text =  mutableStateOf("")
+    val buttonEnabled:Boolean
+        get() = text.value.isNotEmpty()
+
+}
+
 @Composable
 @Preview
-fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
+fun App(appState: AppState) {
+
+
 
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+        Column {
+            TextField(
+                value = appState.text.value,
+                onValueChange = { newTex -> appState.text.value = newTex }
+            )
+            Text(buildMessage(appState.text.value))
+            Button(
+                enabled = appState.buttonEnabled,
+                onClick = {
+                appState.text.value = ""
+            }) {
+                Text("Clean")
+            }
         }
+
     }
 }
 
-fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
-        App()
+fun buildMessage(text: String): String = if (text.isNotEmpty()) "Hello $text" else ""
+
+fun main() {
+    val appState = AppState()
+
+    application {
+        Window(onCloseRequest = ::exitApplication, title = "Application") {
+            App(appState)
+        }
     }
 }
